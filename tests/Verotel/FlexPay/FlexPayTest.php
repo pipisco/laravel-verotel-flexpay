a8bd9e288a0effbd73221e326dc5e92a754b2a28
Dmitry Kovalev <sub@pipisco.local>
2019-05-09 14:40:07 +0300

    Change to PHPUnit TestCase

--- tests/Verotel/FlexPay/FlexPayTest.php
+++ tests/Verotel/FlexPay/FlexPayTest.php
@@ -2,9 +2,9 @@
 
 namespace Pipisco\Tests\Verotel\FlexPay;
 
+use PHPUnit\Framework\TestCase;
 use Pipisco\Verotel\FlexPay\Enums\UrlParameter;
 use Pipisco\Verotel\FlexPay\FlexPayClient;
-use Tests\TestCase;
 
 class FlexPayTest extends TestCase
 {
@@ -24,6 +24,9 @@ class FlexPayTest extends TestCase
     protected $postback_initial_request = 'custom1=Custom+Param+1&custom2=Custom+Param+2&custom3=Custom+Param+3&event=initial&expiresOn=2019-06-09&paymentMethod=CC&period=P1M&priceAmount=12.31&priceCurrency=EUR&referenceID=98040000000000000&saleID=500000&shopID=115790&subscriptionType=one-time&transactionID=100100&type=subscription&signature=7a1a4b6b582743029c4d28667db4b254f35d8ebd';
     protected $postback_rebill_request  = 'amount=12.31&currency=EUR&custom1=Custom+Param+1&custom2=Custom+Param+2&custom3=Custom+Param+3&event=rebill&nextChargeOn=2019-06-30&paymentMethod=CC&referenceID=98040000000000000&saleID=456789&shopID=115790&subscriptionPhase=normal&subscriptionType=recurring&type=subscription&signature=a2a4a35f826faf8b13ec4b09840585245dc40b76';
 
+    /**
+     * @throws \Pipisco\Verotel\FlexPay\FlexPayException
+     */
     public function testInitialPostback()
     {
         $flexpay = new FlexPayClient($this->shop_key, $this->signature_key, $this->version);
@@ -33,6 +36,9 @@ class FlexPayTest extends TestCase
         $this->assertTrue($data[UrlParameter::SIGNATURE] === $postback_initial_request[UrlParameter::SIGNATURE]);
     }
 
+    /**
+     * @throws \Pipisco\Verotel\FlexPay\FlexPayException
+     */
     public function testRebillRequest()
     {
         $flexpay = new FlexPayClient($this->shop_key, $this->signature_key, $this->version);
@@ -41,6 +47,5 @@ class FlexPayTest extends TestCase
 
         $this->assertTrue($data[UrlParameter::SIGNATURE] === $postback_rebill_request[UrlParameter::SIGNATURE]);
     }
-
 
 }
